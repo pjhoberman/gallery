d35fc10c8a81e2b3abde204f0782bbb26ffd6368
PJ Hoberman <pj.hoberman@gmail.com>
2012-05-23 01:15:27 -0600

    some jslint.. more needed

--- gallery.js
+++ gallery.js
@@ -18,62 +18,60 @@ var images = [
     {full: 'test_images/DSC_3795.jpg'},
     {full: 'test_images/DSC_3801.jpg'},
     {full: 'test_images/DSC_3805.jpg'}
-    ];
+];
 
 
-var Gallery = function(){
+var Gallery = function () {
     "use strict";
     var _this = this;
-    this.createGallery = function(){
+    this.createGallery = function () {
         // create the general dom
         $('body').append('<div id="gallery"><div class="big-pic"></div><div class="slider"></div></div>');
-        
+
         // which image should we use
-        var index = 0;
-        
+        var index = 0,
+            hash;
+
         // hashed. load something
-        if( window.location.hash !== '' ){
-            var hash = window.location.hash.match(/#(\d*)-?/);
-            if( hash.length > 1 )
-                index = hash[1];
-            else
-                index = hash[0];
+        if (window.location.hash !== '') {
+            hash = window.location.hash.match(/#(\d*)-?/);
+            index = hash.length > 1 ? hash[1] : hash[0];
         }
-            
+
         $('#gallery .big-pic').append('<img src="' + images[index].full + '" />');
         $('#gallery .big-pic img').one('load', centerBigPic); // look into delay on hashed images
-        
-        
+
+
         // grab the images
         // assumption: we have images
-        for( var i=0; i<images.length; i++){
+        for (var i=0; i<images.length; i++) {
             // check for thumb, otherwise downsize original
             var img = images[i],
                 thumb = img.thumb ? img.thumb : img.full,
                 el = $('<img src="' + thumb + '" />');
-            
-            if( img.title )
+
+            if (img.title)
                 el.attr('title', i + '-' + img.title);
-            
+
             else
                 el.attr('title', i); // for now, assign it its own index -- note, should check for this or a title
-            
-            if( img.link )
+
+            if (img.link)
                 el.attr('data-link', img.link);
-            
+
             $('#gallery .slider').append(el);
-                
+
         } // for
-        
+
         $('#gallery .slider img:eq(' + index + ')').addClass('selected-image');
-        
+
         // bind the click
         $('#gallery .slider img').click(function(){
             $('.selected-image').removeClass('selected-image');
             $(this).addClass('selected-image');
             _this.changeBigPic($(this));
         });
-        
+
         // slider craziness - not working right now, hovering on right isn't a consistent e.offsetX
 /*
         $('#gallery .slider').mousemove(function(e){
@@ -81,39 +79,39 @@ var Gallery = function(){
             console.log(e);
             console.log(slider.width());
             console.log(e.offsetX);
-            if( e.offsetX < (slider.width() * .25))
+            if(e.offsetX < (slider.width() * .25))
                 console.log( 'slide left' );
             else if( e.offsetX > (slider.width() * .75))
                 console.log( 'slide right');
         });
 */
     };
-    
-    this.changeBigPic = function(img){
+
+    this.changeBigPic = function (img) {
         // center vertically
-        $('#gallery .big-pic img').attr('src', img.attr('src')).one('load', centerBigPic );
-        
+        $('#gallery .big-pic img').attr('src', img.attr('src')).one('load', centerBigPic);
+
         // update the hash
         window.location.hash = img.attr('title');
     };
-    
+
     // private function because.. no reason to ever use this outside the obj?
-    var centerBigPic = function(){
+    var centerBigPic = function () {
         var image_height = $($('#gallery .big-pic img')[0]).height(),
             max_height = $('#gallery .big-pic').height(),
             margin = Math.max(0, (max_height - image_height)/2);
-        
+
         $('#gallery .big-pic img').css({marginTop:margin});
     };
-    
-    
+
+
     // init functions
     this.createGallery();
 };
 
 $(function(){
     "use strict";
-    
+
     var gallery = new Gallery();
-    
+
 }); // doc ready
